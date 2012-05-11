ae081f0b4c0217b334777c64513cbca589c0d82c
Paul Sarando <psarando@iplantcollaborative.org>
2012-05-11 15:56:12 -0700

    Removed some PostgreSQL dump settings.
    Removed some (unnecessary?) PostgreSQL dump administrivia.

--- src/main/sql/org/iplantc/sql/master.dump.sql
+++ src/main/sql/org/iplantc/sql/master.dump.sql
@@ -2,13 +2,6 @@
 -- PostgreSQL database dump
 --
 
-SET statement_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = off;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET escape_string_warning = off;
-
 SET search_path = public, pg_catalog;
 
 --
@@ -31,10 +24,6 @@ ALTER TABLE public.template_group_id_seq OWNER TO de;
 
 SELECT pg_catalog.setval('template_group_id_seq', 42, true);
 
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
 
 --
 -- Name: template_group; Type: TABLE; Schema: public; Owner: de; Tablespace: 
