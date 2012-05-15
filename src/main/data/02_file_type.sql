2454b9cae9c0a8be62b0a0bb3b1861a0a1a60faa
Dennis Roberts <dennis@iplantcollaborative.org>
2012-05-14 17:55:08 -0700

    CORE-3168: added the data definitions

---
+++ src/main/data/02_file_type.sql
@@ -0,0 +1,25 @@
+SET search_path = public, pg_catalog;
+
+--
+-- Data for Name: file_type; Type: TABLE DATA; Schema: public; Owner: de
+--
+COPY file_type (id, description) FROM stdin;
+0	Unknown
+1	Newick
+2	NEXUS with unrecognized data
+3	NEXUS with tree data
+4	NEXUS with trait data
+5	NEXUS with tree and trait data
+6	CSV trait data
+7	Text file
+8	FASTA file
+9	FASTQ file
+10	SAM file
+11	BAM file
+12	Pileup file
+13	VCF 3.3 file
+14	Barcode file
+15	CSV Name List
+16	Taxamatch Result
+17	Taxamatch Name List
+\.
---
+++ src/main/data/08_rule_type_value_type.sql
@@ -0,0 +1,19 @@
+SET search_path = public, pg_catalog;
+
+--
+-- Data for Name: rule_type_value_type; Type: TABLE DATA; Schema: public; Owner: de
+--
+COPY rule_type_value_type (rule_type_id, value_type_id) FROM stdin;
+2	3
+1	3
+4	3
+3	3
+7	3
+8	3
+9	3
+10	1
+11	1
+5	1
+5	3
+6	3
+\.
