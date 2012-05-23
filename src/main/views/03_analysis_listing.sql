0cca16c34bb2b568577178bec2a99f539f70b075
Dennis Roberts <dennis@iplantcollaborative.org>
2012-05-23 13:57:18 -0700

    fixed the analysis_listing table

--- src/main/views/03_analysis_listing.sql
+++ src/main/views/03_analysis_listing.sql
@@ -7,13 +7,13 @@ SET search_path = public, pg_catalog;
 CREATE VIEW analysis_listing AS
     SELECT analysis.hid,
            analysis.id,
-           analysis."name",
+           analysis.name,
            analysis.description,
            integration.integrator_name,
            integration.integrator_email,
            analysis.integration_date,
            analysis.wikiurl,
-           CAST(COALESCE(AVG(ratings.rating), 0.0) AS DOUBLE PRECISION)
+           (COALESCE(avg(ratings.rating), 0.0))::double precision
                AS average_rating,
            EXISTS (
                SELECT *
@@ -23,20 +23,23 @@ CREATE VIEW analysis_listing AS
                WHERE analysis.hid = tgt.template_id
                AND w.is_public IS TRUE
            ) AS is_public, (
-               SELECT COUNT(*)
+               SELECT count(*) AS count
                FROM transformation_task_steps tts
                WHERE tts.transformation_task_id = analysis.hid
-           ) AS step_count
+           ) AS step_count,
+           analysis.deleted,
+           analysis.disabled
     FROM transformation_activity analysis
-         LEFT JOIN integration_data integration
-              ON analysis.integration_data_id = integration.id
-         LEFT JOIN ratings ON analysis.hid = ratings.transformation_activity_id
-    WHERE analysis.deleted IS FALSE
+        LEFT JOIN integration_data integration
+            ON analysis.integration_data_id = integration.id
+        LEFT JOIN ratings ON analysis.hid = ratings.transformation_activity_id
     GROUP BY analysis.hid,
              analysis.id,
-             analysis."name",
+             analysis.name,
              analysis.description,
              integration.integrator_name,
              integration.integrator_email,
              analysis.integration_date,
-             analysis.wikiurl;
+             analysis.wikiurl,
+             analysis.deleted,
+             analysis.disabled;
