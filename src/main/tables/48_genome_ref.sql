8b036227ade3969190fd3d0c4ea35c81220774b4
Roey Chasman <rchasman@email.arizona.edu>
2012-05-29 14:27:13 -0700

    Fixing bugs (Does not compile with facepalm)

--- src/main/tables/48_genome_ref.sql
+++ src/main/tables/48_genome_ref.sql
@@ -3,25 +3,20 @@ SET search_path = public, pg_catalog;
 --
 -- hid SERIAL type for genome_ref table
 --
-CREATE SEQUENCE genome_ref_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MAXVALUE
-    NO MINVALUE
-    CACHE 1;
+CREATE SEQUENCE genome_ref_id_seq;
 
 --
 -- template_group table
 --
 CREATE TABLE genome_reference (
-    id bigint DEFAULT nextval('genome_ref_id_seq'::regclass) NOT NULL,
+    id bigint DEFAULT nextval('genome_ref_id_seq'::regclass),
     uuid bigint NOT NULL,
     path character varying(1024) NOT NULL,
     deleted boolean NOT NULL,
     created_by bigint references users(id),
-    created_on date NOT NULL,
+    created_on timestamp DEFAULT now(),
     last_modified_by bigint references users(id),
-    last_modified_on date,
+    last_modified_on timestamp,
     PRIMARY KEY (id)
 );
 
