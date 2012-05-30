32c5efbf5c78b706a4ae622d6170a7589230b79c
Roey Chasman <rchasman@email.arizona.edu>
2012-05-30 11:21:26 -0700

    Created a file to populate the new genome_reference table with default genome metadata from metadactl.

---
+++ src/main/data/12_ref_genomes.sql
@@ -0,0 +1,7 @@
+--This is the reference_genomes.json from metadactly to be autopopulated in the
+--postgresDB in the genome_reference table.
+
+INSERT INTO genome_reference
+(uuid, name, path, deleted, created_by)
+VALUES
+('54DC0C16-226C-459A-B186-5E570228A130','<name>','<path>','<deleted>','<created_by>');
