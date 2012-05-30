575ac75f2e748487ebe15aa80015d8d80bf081d3
Roey Chasman <rchasman@email.arizona.edu>
2012-05-30 15:02:14 -0700

    Final syntax editing

--- src/main/data/12_ref_genomes.sql
+++ src/main/data/12_ref_genomes.sql
@@ -14,51 +14,51 @@ VALUES ('61DC0C16-226C-458A-B186-5E570258A130','Bos taurus UMD3.1 (Ensembl 66)',
     '/data2/collections/genomeservices/0.2/Bos_taurus.UMD3.1/de_support/',0);
 
 INSERT INTO genome_reference (uuid, name, path, created_by)
-VALUES ('54DC1C16-223C-458A-B186-5E570230A130','Brachypodium distachyon Brachy1.0 (Ensembl 13)',
+VALUES ('ABF7B800-21BE-44EB-84DB-0F87E7094B27','Brachypodium distachyon Brachy1.0 (Ensembl 13)',
     '/data2/collections/genomeservices/0.2/Brachypodium_distachyon.Brachy1.0/de_support/',0);
 
 INSERT INTO genome_reference (uuid, name, path, created_by)
-VALUES ('54DC1C16-225C-457A-B186-5E570229A130','Brassica rapa IVFCAASv1 (Ensembl 13)',
+VALUES ('FCE82CED-6E9F-4909-B360-AA7FE0072DD7','Brassica rapa IVFCAASv1 (Ensembl 13)',
     '/data2/collections/genomeservices/0.2/Brassica_rapa.IVFCAASv1/de_support/',0);
 
 INSERT INTO genome_reference (uuid, name, path, created_by)
-VALUES ('70DC0C16-225C-458A-B186-5E570229A130','Caenorhabditis elegans WS220 (Ensembl 66)',
+VALUES ('806B6D5A-DF12-448B-BC14-206F1543F13F','Caenorhabditis elegans WS220 (Ensembl 66)',
     '/data2/collections/genomeservices/0.2/Caenorhabditis_elegans.WS220/de_support/',0);
 
 INSERT INTO genome_reference (uuid, name, path, created_by)
-VALUES ('56DC0C16-222C-458A-B186-5E570228A130','Canis familiaris BROADD2 (Ensembl 66)',
+VALUES ('B46C7DAE-9521-43DA-A7DA-8BDB76BE06FA','Canis familiaris BROADD2 (Ensembl 66)',
     '/data2/collections/genomeservices/0.2/Canis_familiaris.BROADD2/de_support/',0);
 
 INSERT INTO genome_reference (uuid, name, path, created_by)
-VALUES ('56DC0C17-226C-458A-B186-5E570230A130','Chlamydomonas reinhardtii ENA1 (Ensembl 13)',
+VALUES ('B7FE13D8-A9E0-408C-946F-5E3AFB922E7D','Chlamydomonas reinhardtii ENA1 (Ensembl 13)',
     '/data2/collections/genomeservices/0.2/Chlamydomonas_reinhardtii.ENA1/de_support/',0);
 
 INSERT INTO genome_reference (uuid, name, path, created_by)
-VALUES ('58DC9C16-214C-450A-B187-5E570229A130','Cyanidioschyzon merolae ENA1 (Ensembl 13)',
+VALUES ('F1BE1410-9C90-41D1-84DD-04B674E488CF','Cyanidioschyzon merolae ENA1 (Ensembl 13)',
     '/data2/collections/genomeservices/0.2/Cyanidioschyzon_merolae.ENA1/de_support/',0);
 
 INSERT INTO genome_reference (uuid, name, path, created_by)
-VALUES ('80DC1C16-225C-454A-B188-5E570229A130','Danio rerio Zv9 (Ensembl 66)',
+VALUES ('2EF5D109-BF75-4497-AD82-701852D274A3','Danio rerio Zv9 (Ensembl 66)',
     '/data2/collections/genomeservices/0.2/Danio_rerio.Zv9/de_support/',0);
 
 INSERT INTO genome_reference (uuid, name, path, created_by)
-VALUES ('80DC1C16-224C-458A-B189-5E570229A130','Drosophila melanogaster BDGP5 (Ensembl 66)',
+VALUES ('0CF5BA6C-9DE6-4D1D-A909-66FC69F6A4E8','Drosophila melanogaster BDGP5 (Ensembl 66)',
     '/data2/collections/genomeservices/0.2/Drosophila_melanogaster.BDGP5/de_support/',0);
 
 INSERT INTO genome_reference (uuid, name, path, created_by)
-VALUES ('80DC1C16-325C-459A-B186-5E570229A134','Felis catus CAT (Ensembl 66)',
+VALUES ('F0F4D4F7-7782-41BF-8986-A161879CF3EC','Felis catus CAT (Ensembl 66)',
     '/data2/collections/genomeservices/0.2/Felis_catus.CAT/de_support/',0);
 
 INSERT INTO genome_reference (uuid, name, path, created_by)
-VALUES ('80DC1C16-179C-458A-B186-5E570229A131','Glycine max 1.0 (Ensembl 13)',
+VALUES ('E4C89C48-84E4-487F-BE14-427562EFB5A4','Glycine max 1.0 (Ensembl 13)',
     '/data2/collections/genomeservices/0.2/Glycine_max.1.0/de_support/',0);
 
 INSERT INTO genome_reference (uuid, name, path, created_by)
-VALUES ('80DC1C16-221C-459A-B186-5E570229A130','Homo sapiens GRCh37 (Ensembl 66)',
+VALUES ('0D5FD2AC-69E7-4E5C-B0FE-4B7BD2EA9348','Homo sapiens GRCh37 (Ensembl 66)',
     '/data2/collections/genomeservices/0.2/Homo_sapiens.GRCh37/de_support/',0);
 
 INSERT INTO genome_reference (uuid, name, path, created_by)
-VALUES ('80DC1C16-225C-459A-B186-5E570229A130','Mus musculus NCBIM37 (Ensembl 66)',
+VALUES ('7FFE68A5-70BE-44D6-8A48-236D2E2BAF7A','Mus musculus NCBIM37 (Ensembl 66)',
     '/data2/collections/genomeservices/0.2/Mus_musculus.NCBIM37/de_support/',0);
 
 INSERT INTO genome_reference (uuid, name, path, created_by)
