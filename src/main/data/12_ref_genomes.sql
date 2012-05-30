e1e5e44bded3214006af7ffaa053d552110a2f6d
Roey Chasman <rchasman@email.arizona.edu>
2012-05-30 12:02:42 -0700

    removed columns that don't need to be added manually, and began entering a few data points.

--- src/main/data/12_ref_genomes.sql
+++ src/main/data/12_ref_genomes.sql
@@ -2,6 +2,16 @@
 --postgresDB in the genome_reference table.
 
 INSERT INTO genome_reference
-(uuid, name, path, deleted, created_by)
+(uuid, name, path, created_by)
 VALUES
-('54DC0C16-226C-459A-B186-5E570228A130','<name>','<path>','<deleted>','<created_by>');
+('54DC0C16-226C-459A-B186-5E570228A130','Arabidopsis lyrata Araly1 (Ensembl 13)','/data2/collections/genomeservices/0.2/Arabidopsis_lyrata.Araly1/de_support/',0)
+('54DC0C16-226C-458A-B186-5E570228A130','Arabidopsis thaliana TAIR10 (Ensembl 13)','/data2/collections/genomeservices/0.2/Arabidopsis_thaliana.TAIR10/de_support/',0)
+('61DC0C16-226C-458A-B186-5E570258A130','Bos taurus UMD3.1 (Ensembl 66)','/data2/collections/genomeservices/0.2/Bos_taurus.UMD3.1/de_support/',0)
+('54DC1C16-225C-459A-B186-5E570230A130','<name>','<path>',0)
+('54DC1C16-226C-458A-B186-5E570229A130','<name>','<path>',0)
+('70DC0C16-226C-459A-B186-5E570229A130','<name>','<path>',0)
+('56DC0C16-226C-459A-B186-5E570228A130','<name>','<path>',0)
+('56DC0C17-226C-459A-B186-5E570230A130','<name>','<path>',0)
+('58DC9C16-214C-455A-B187-5E570229A130','<name>','<path>',0)
+('80DC1C16-225C-459A-B186-5E570229A130','<name>','<path>',0)
+;
