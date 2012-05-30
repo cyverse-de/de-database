18e446a341c47655f3486efa776d44172e2922f7
Roey Chasman <rchasman@email.arizona.edu>
2012-05-30 14:24:30 -0700

    More syntax editing, learned some sweet vim tricks from Paul to make this a snap.

--- src/main/data/12_ref_genomes.sql
+++ src/main/data/12_ref_genomes.sql
@@ -1,35 +1,118 @@
 --This is the reference_genomes.json from metadactly to be autopopulated in the
 --postgresDB in the genome_reference table.
 
-INSERT INTO genome_reference
-(uuid, name, path, created_by)
-VALUES
-('54DC0C16-226C-459A-B186-5E570228A130','Arabidopsis lyrata Araly1 (Ensembl 13)','/data2/collections/genomeservices/0.2/Arabidopsis_lyrata.Araly1/de_support/',0),
-('54DC0C16-226C-458A-B186-5E570228A130','Arabidopsis thaliana TAIR10 (Ensembl 13)','/data2/collections/genomeservices/0.2/Arabidopsis_thaliana.TAIR10/de_support/',0),
-('61DC0C16-226C-458A-B186-5E570258A130','Bos taurus UMD3.1 (Ensembl 66)','/data2/collections/genomeservices/0.2/Bos_taurus.UMD3.1/de_support/',0),
-('54DC1C16-225C-459A-B186-5E570230A130','Brachypodium distachyon Brachy1.0 (Ensembl 13)','/data2/collections/genomeservices/0.2/Brachypodium_distachyon.Brachy1.0/de_support/',0),
-('54DC1C16-226C-458A-B186-5E570229A130','Brassica rapa IVFCAASv1 (Ensembl 13)','/data2/collections/genomeservices/0.2/Brassica_rapa.IVFCAASv1/de_support/',0),
-('70DC0C16-226C-459A-B186-5E570229A130','Caenorhabditis elegans WS220 (Ensembl 66)','/data2/collections/genomeservices/0.2/Caenorhabditis_elegans.WS220/de_support/',0),
-('56DC0C16-226C-459A-B186-5E570228A130','Canis familiaris BROADD2 (Ensembl 66)','/data2/collections/genomeservices/0.2/Canis_familiaris.BROADD2/de_support/',0),
-('56DC0C17-226C-459A-B186-5E570230A130','Chlamydomonas reinhardtii ENA1 (Ensembl 13)','/data2/collections/genomeservices/0.2/Chlamydomonas_reinhardtii.ENA1/de_support/',0),
-('58DC9C16-214C-455A-B187-5E570229A130','Cyanidioschyzon merolae ENA1 (Ensembl 13)','/data2/collections/genomeservices/0.2/Cyanidioschyzon_merolae.ENA1/de_support/',0),
-('80DC1C16-225C-459A-B186-5E570229A130','Danio rerio Zv9 (Ensembl 66)','/data2/collections/genomeservices/0.2/Danio_rerio.Zv9/de_support/',0),
-('80DC1C16-225C-459A-B186-5E570229A130','Drosophila melanogaster BDGP5 (Ensembl 66)','/data2/collections/genomeservices/0.2/Drosophila_melanogaster.BDGP5/de_support/',0),
-('80DC1C16-225C-459A-B186-5E570229A130','Felis catus CAT (Ensembl 66)','/data2/collections/genomeservices/0.2/Felis_catus.CAT/de_support/',0),
-('80DC1C16-225C-459A-B186-5E570229A130','Glycine max 1.0 (Ensembl 13)','/data2/collections/genomeservices/0.2/Glycine_max.1.0/de_support/',0),
-('80DC1C16-225C-459A-B186-5E570229A130','Homo sapiens GRCh37 (Ensembl 66)','/data2/collections/genomeservices/0.2/Homo_sapiens.GRCh37/de_support/',0),
-('80DC1C16-225C-459A-B186-5E570229A130','Mus musculus NCBIM37 (Ensembl 66)','/data2/collections/genomeservices/0.2/Mus_musculus.NCBIM37/de_support/',0),
-('80DC1C16-225C-459A-B186-5E570229A130','Oryza glaberrima AGI1.1 (Ensembl 13)','/data2/collections/genomeservices/0.2/Oryza_glaberrima.AGI1.1/de_support/',0),
-('80DC1C16-225C-459A-B186-5E570229A130','Oryza indica Jan_2005 (Ensembl 13)','/data2/collections/genomeservices/0.2/Oryza_indica.Jan_2005/de_support/',0),
-('80DC1C16-225C-459A-B186-5E570229A130','Oryza sativa MSU6 (Ensembl 13)','/data2/collections/genomeservices/0.2/Oryza_sativa.MSU6/de_support/',0),
-('80DC1C16-225C-459A-B186-5E570229A130','Pan troglodytes CHIMP2.1.4 (Ensembl 66)','/data2/collections/genomeservices/0.2/Pan_troglodytes.CHIMP2.1.4/de_support/',0),
-('80DC1C16-225C-459A-B186-5E570229A130','Physcomitrella patens Phypa1.1 (Ensembl 13)','/data2/collections/genomeservices/0.2/Physcomitrella_patens.Phypa1.1/de_support/',0),
-('80DC1C16-225C-459A-B186-5E570229A130','Populus trichocarpa JGI2.0 (Ensembl 13)','/data2/collections/genomeservices/0.2/Populus_trichocarpa.JGI2.0/de_support/',0)
-('80DC1C16-225C-459A-B186-5E570229A130','Rattus norvegicus RGSC3.4 (Ensembl 66)','/data2/collections/genomeservices/0.2/Rattus_norvegicus.RGSC3.4/de_support/',0),
-('80DC1C16-225C-459A-B186-5E570229A130','Saccharomyces cerevisiae EF4 (Ensembl 66)','/data2/collections/genomeservices/0.2/Saccharomyces_cerevisiae.EF4/de_support/',0),
-('80DC1C16-225C-459A-B186-5E570229A130','Selaginella moellendorffii ENA1 (Ensembl 13)','/data2/collections/genomeservices/0.2/Selaginella_moellendorffii.ENA1/de_support/',0),
-('80DC1C16-225C-459A-B186-5E570229A130','Sorghum bicolor Sbi1 (Ensembl 13)','/data2/collections/genomeservices/0.2/Sorghum_bicolor.Sbi1/de_support/',0),
-('80DC1C16-225C-459A-B186-5E570229A130','Tursiops truncatus turTru1 (Ensembl 66)','/data2/collections/genomeservices/0.2/Tursiops_truncatus.turTru1/de_support/',0),
-('80DC1C16-225C-459A-B186-5E570229A130','Vitis vinifera IGGP_12x (Ensembl 13)','/data2/collections/genomeservices/0.2/Vitis_vinifera.IGGP_12x/de_support/',0),
-('80DC1C16-225C-459A-B186-5E570229A130','Xenopus tropicalis JGI_4.2 (Ensembl 66)','/data2/collections/genomeservices/0.2/Xenopus_tropicalis.JGI_4.2/de_support/',0),
-('80DC1C16-225C-459A-B186-5E570229A130','Zea mays AGPv2 (Ensembl 13)','/data2/collections/genomeservices/0.2/Zea_mays.AGPv2/de_support/',0);
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('54DC0C16-226C-459A-B186-5E570228A130','Arabidopsis lyrata Araly1 (Ensembl 13)',
+    '/data2/collections/genomeservices/0.2/Arabidopsis_lyrata.Araly1/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('54DC0C16-226C-458A-B186-5E570228A130','Arabidopsis thaliana TAIR10 (Ensembl 13)',
+    '/data2/collections/genomeservices/0.2/Arabidopsis_thaliana.TAIR10/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('61DC0C16-226C-458A-B186-5E570258A130','Bos taurus UMD3.1 (Ensembl 66)',
+    '/data2/collections/genomeservices/0.2/Bos_taurus.UMD3.1/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('54DC1C16-223C-458A-B186-5E570230A130','Brachypodium distachyon Brachy1.0 (Ensembl 13)',
+    '/data2/collections/genomeservices/0.2/Brachypodium_distachyon.Brachy1.0/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('54DC1C16-225C-457A-B186-5E570229A130','Brassica rapa IVFCAASv1 (Ensembl 13)',
+    '/data2/collections/genomeservices/0.2/Brassica_rapa.IVFCAASv1/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('70DC0C16-225C-458A-B186-5E570229A130','Caenorhabditis elegans WS220 (Ensembl 66)',
+    '/data2/collections/genomeservices/0.2/Caenorhabditis_elegans.WS220/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('56DC0C16-222C-458A-B186-5E570228A130','Canis familiaris BROADD2 (Ensembl 66)',
+    '/data2/collections/genomeservices/0.2/Canis_familiaris.BROADD2/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('56DC0C17-226C-458A-B186-5E570230A130','Chlamydomonas reinhardtii ENA1 (Ensembl 13)',
+    '/data2/collections/genomeservices/0.2/Chlamydomonas_reinhardtii.ENA1/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('58DC9C16-214C-450A-B187-5E570229A130','Cyanidioschyzon merolae ENA1 (Ensembl 13)',
+    '/data2/collections/genomeservices/0.2/Cyanidioschyzon_merolae.ENA1/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('80DC1C16-225C-454A-B188-5E570229A130','Danio rerio Zv9 (Ensembl 66)',
+    '/data2/collections/genomeservices/0.2/Danio_rerio.Zv9/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('80DC1C16-224C-458A-B189-5E570229A130','Drosophila melanogaster BDGP5 (Ensembl 66)',
+    '/data2/collections/genomeservices/0.2/Drosophila_melanogaster.BDGP5/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('80DC1C16-325C-459A-B186-5E570229A134','Felis catus CAT (Ensembl 66)',
+    '/data2/collections/genomeservices/0.2/Felis_catus.CAT/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('80DC1C16-179C-458A-B186-5E570229A131','Glycine max 1.0 (Ensembl 13)',
+    '/data2/collections/genomeservices/0.2/Glycine_max.1.0/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('80DC1C16-221C-459A-B186-5E570229A130','Homo sapiens GRCh37 (Ensembl 66)',
+    '/data2/collections/genomeservices/0.2/Homo_sapiens.GRCh37/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('80DC1C16-225C-459A-B186-5E570229A130','Mus musculus NCBIM37 (Ensembl 66)',
+    '/data2/collections/genomeservices/0.2/Mus_musculus.NCBIM37/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('D09C1C16-225C-459A-B187-5E570229A130','Oryza glaberrima AGI1.1 (Ensembl 13)',
+    '/data2/collections/genomeservices/0.2/Oryza_glaberrima.AGI1.1/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('80DC1C16-425C-459A-B186-5E5702363130','Oryza indica Jan_2005 (Ensembl 13)',
+    '/data2/collections/genomeservices/0.2/Oryza_indica.Jan_2005/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('802C1C16-225C-459A-BR88-5E570229A130','Oryza sativa MSU6 (Ensembl 13)',
+    '/data2/collections/genomeservices/0.2/Oryza_sativa.MSU6/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('801C1C16-725C-458A-B187-5E570229A130','Pan troglodytes CHIMP2.1.4 (Ensembl 66)',
+    '/data2/collections/genomeservices/0.2/Pan_troglodytes.CHIMP2.1.4/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('F0D31C16-224C-457A-B188-5E570229A130','Physcomitrella patens Phypa1.1 (Ensembl 13)',
+    '/data2/collections/genomeservices/0.2/Physcomitrella_patens.Phypa1.1/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('80DC1C16-725C-457A-B18R-5E570229A130','Populus trichocarpa JGI2.0 (Ensembl 13)',
+    '/data2/collections/genomeservices/0.2/Populus_trichocarpa.JGI2.0/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('R0DC3C16-825C-459A-B189-5R570229A130','Rattus norvegicus RGSC3.4 (Ensembl 66)',
+    '/data2/collections/genomeservices/0.2/Rattus_norvegicus.RGSC3.4/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('80DC1C16-225C-459A-B188-5E570229A130','Saccharomyces cerevisiae EF4 (Ensembl 66)',
+    '/data2/collections/genomeservices/0.2/Saccharomyces_cerevisiae.EF4/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('R0DC1C16-925C-458A-B186-5R570229A130','Selaginella moellendorffii ENA1 (Ensembl 13)',
+    '/data2/collections/genomeservices/0.2/Selaginella_moellendorffii.ENA1/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('80DC1C17-224C-457A-B186-5E570229A130','Sorghum bicolor Sbi1 (Ensembl 13)',
+    '/data2/collections/genomeservices/0.2/Sorghum_bicolor.Sbi1/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('80DC1C16-225C-459A-B186-5E570229A130','Tursiops truncatus turTru1 (Ensembl 66)',
+    '/data2/collections/genomeservices/0.2/Tursiops_truncatus.turTru1/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('8RDC1C18-225C-459A-B186-5E570229A130','Vitis vinifera IGGP_12x (Ensembl 13)',
+    '/data2/collections/genomeservices/0.2/Vitis_vinifera.IGGP_12x/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('80DC3C16-225C-459A-B186-5E570229A130','Xenopus tropicalis JGI_4.2 (Ensembl 66)',
+    '/data2/collections/genomeservices/0.2/Xenopus_tropicalis.JGI_4.2/de_support/',0);
+
+INSERT INTO genome_reference (uuid, name, path, created_by)
+VALUES ('86DC2C16-225C-459A-B186-5E570229A130','Zea mays AGPv2 (Ensembl 13)',
+    '/data2/collections/genomeservices/0.2/Zea_mays.AGPv2/de_support/',0);
