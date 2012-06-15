3a8dd62c14339fb5faf7d6b72ce0074b33ff8d06
Dennis Roberts <dennis@iplantcollaborative.org>
2012-06-15 16:04:50 -0700

    CORE-3251: updated the database version number

--- src/main/data/99_version.sql
+++ src/main/data/99_version.sql
@@ -1,3 +1,3 @@
 INSERT INTO version (version) VALUES ('1.4.0:20120525.01');
 INSERT INTO version (version) VALUES ('1.4.0:20120530.01');
-
+INSERT INTO version (version) VALUES ('1.4.0:20120615.01');
