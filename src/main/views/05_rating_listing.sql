eeb8a4f9d38a6d349b2005263852d4155f58f37c
Dennis Roberts <dennis@iplantcollaborative.org>
2012-07-18 16:30:56 -0700

    CORE-3373: redefine rating_listing to get rid of the cross join

--- src/main/views/05_rating_listing.sql
+++ src/main/views/05_rating_listing.sql
@@ -5,12 +5,11 @@ SET search_path = public, pg_catalog;
 -- listing service.
 --
 CREATE VIEW rating_listing AS
-    SELECT row_number() OVER (ORDER BY analysis.hid, u.id) AS id,
-           analysis.hid AS analysis_id,
+    SELECT row_number() OVER (ORDER BY a.hid, u.id) AS id,
+           a.hid AS analysis_id,
            u.id AS user_id,
            ur.comment_id AS comment_id,
            ur.rating AS user_rating
-    FROM transformation_activity analysis
-         CROSS JOIN users u
-         LEFT JOIN ratings ur ON analysis.hid = ur.transformation_activity_id
-             AND ur.user_id = u.id;
+    FROM ratings ur
+    JOIN users u ON ur.user_id = u.id
+    JOIN transformation_activity a ON a.hid = ur.transformation_activity_id;
