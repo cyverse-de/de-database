8fbb08ba37569c13fe9e67ef2cbb85ec4e1a2453
Dennis Roberts <dennis@iplantcollaborative.org>
2012-07-20 18:38:50 -0700

    CORE-3373: add some functions that can be used to efficiently list all app groups in an app group hierarchy

--- src/main/data/99_version.sql
+++ src/main/data/99_version.sql
@@ -2,4 +2,4 @@ INSERT INTO version (version) VALUES ('1.4.0:20120525.01');
 INSERT INTO version (version) VALUES ('1.4.0:20120530.01');
 INSERT INTO version (version) VALUES ('1.4.0:20120615.01');
 INSERT INTO version (version) VALUES ('1.4.0:20120618.01');
-
+INSERT INTO version (version) VALUES ('1.4.0:20120720.01');
