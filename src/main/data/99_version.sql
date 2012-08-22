d4861c9bf02fd212bf0939bb4e53f607ca77cbe1
Dennis Roberts <dennis@iplantcollaborative.org>
2012-08-22 09:56:00 -0700

    CORE-3453: added another entry to the version table

--- src/main/data/99_version.sql
+++ src/main/data/99_version.sql
@@ -4,3 +4,4 @@ INSERT INTO version (version) VALUES ('1.4.0:20120615.01');
 INSERT INTO version (version) VALUES ('1.4.0:20120618.01');
 INSERT INTO version (version) VALUES ('1.4.0:20120720.01');
 INSERT INTO version (version) VALUES ('1.4.0:20120726.01');
+INSERT INTO version (version) VALUES ('1.4.0:20120822.01');
