2539950f70f2f8f6ad5b399fa4e298199727945b
Paul Sarando <psarando@iplantcollaborative.org>
2012-10-22 12:37:38 -0700

    CORE-3517 Added another entry to version table.

--- src/main/data/99_version.sql
+++ src/main/data/99_version.sql
@@ -5,3 +5,5 @@ INSERT INTO version (version) VALUES ('1.4.0:20120618.01');
 INSERT INTO version (version) VALUES ('1.4.0:20120720.01');
 INSERT INTO version (version) VALUES ('1.4.0:20120726.01');
 INSERT INTO version (version) VALUES ('1.4.0:20120822.01');
+INSERT INTO version (version) VALUES ('1.4.4:20120927.01');
+
