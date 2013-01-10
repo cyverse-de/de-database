20ed1eb8a883a262c3f8c943ce1b019ebd51563d
Dennis Roberts <dennis@iplantcollaborative.org>
2013-01-09 17:39:59 -0700

    CORE-3866: initial commit

---
+++ src/main/tables/01_notifications.sql
@@ -0,0 +1,26 @@
+--
+-- ID sequence for the notifications table.
+--
+CREATE SEQUENCE notifications_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+--
+-- Stores notification information.
+--
+CREATE TABLE notifications (
+    id BIGINT DEFAULT nextval('notification_id_seq'::regclass) NOT NULL,
+    uuid UUID NOT NULL,
+    type VARCHAR(32),
+    user VARCHAR(512),
+    subject VARCHAR(255),
+    seen BOOLEAN,
+    deleted BOOLEAN,
+    date_created TIMESTAMP DEFAULT now(),
+    message TEXT,
+    PRIMARY KEY(id)
+);
