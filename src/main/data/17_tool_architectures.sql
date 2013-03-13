148ab05fe702ac37fca3145213b47216796a9e6f
Dennis Roberts <dennis@iplantcollaborative.org>
2013-03-13 14:47:24 -0700

    CORE-4031: added two new options for tool architectures

--- src/main/data/17_tool_architectures.sql
+++ src/main/data/17_tool_architectures.sql
@@ -2,4 +2,6 @@
 
 INSERT INTO tool_architectures ( name, description ) VALUES
     ( '32-bit Generic', '32-bit executables on an unspecified architecture.' ),
-    ( '64-bit Generic', '64-bit executables on an unspecified architecture.' );
+    ( '64-bit Generic', '64-bit executables on an unspecified architecture.' ),
+    ( 'Others', 'Another specific architecture.' )
+    ( 'Don''t know', 'Used in cases where the user doesn''t know the architecture.' );
