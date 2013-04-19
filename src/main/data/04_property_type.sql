3f8b454e779a145fd984d78e6aa715043db9c7b2
Dennis Roberts <dennis@iplantcollaborative.org>
2013-04-19 13:42:07 -0700

    Property Type Changes:
    - Added several new property types.
    - Deprecated some older property types.
    - Reordered existing property types.

--- src/main/data/04_property_type.sql
+++ src/main/data/04_property_type.sql
@@ -26,7 +26,7 @@ INSERT INTO property_type
 INSERT INTO property_type
     (hid, id, name, description, label, deprecated, display_order, value_type_id)
     VALUES ('6', 'ptc5b85c6b-381e-44f6-a568-186f1fe7f03d', 'Info',
-            'Informative static text', NULL, TRUE, '8', '1');
+            'Informative static text', NULL, TRUE, '999', '1');
 
 INSERT INTO property_type
     (hid, id, name, description, label, deprecated, display_order, value_type_id)
@@ -36,63 +36,92 @@ INSERT INTO property_type
 INSERT INTO property_type
     (hid, id, name, description, label, deprecated, display_order, value_type_id)
     VALUES ('1', 'ptd2340f11-d260-41b4-93fd-c1d695bf6fef', 'Number',
-            'A text box that checks for valid number input', NULL, FALSE, '2', '3');
+            'A text box that checks for valid number input', NULL, TRUE, '999',
+            '3');
 
 INSERT INTO property_type
     (hid, id, name, description, label, deprecated, display_order, value_type_id)
     VALUES ('10', 'ptf22ca553-856b-4253-b0f3-514701ed4567', 'QuotedText',
             'A text box that will add quotes for passing string to command line',
-            NULL, TRUE, '3', '1');
+            NULL, TRUE, '999', '1');
 
 INSERT INTO property_type
     (hid, id, name, description, label, deprecated, display_order, value_type_id)
     VALUES ('5', 'pt206a93d6-bac4-4925-89fe-39c073e85c47', 'Flag',
-            'A checkbox for yes/no selection', NULL, FALSE, '5', '2');
+            'A checkbox for yes/no selection', NULL, FALSE, '3', '2');
 
 INSERT INTO property_type
     (hid, id, name, description, label, deprecated, display_order, value_type_id)
     VALUES ('8', 'ptbabc3c29-39c2-47b5-8576-f3741f9ae329', 'Selection',
-            'A list for selecting a choice (can be text)', NULL, FALSE, '6', '1');
+            'A list for selecting a choice (can be text)', NULL, TRUE, '999',
+            '1');
 
 INSERT INTO property_type
     (hid, id, name, description, label, deprecated, display_order, value_type_id)
     VALUES ('9', 'pt7c71012b-158d-44fd-bda1-a5fb4d43bfd8', 'ValueSelection',
-            'A list for selecting a value (numeric range)', NULL, FALSE, '7', '3');
-
-INSERT INTO property_type
-    (hid, id, name, description, label, deprecated, display_order, value_type_id)
-    VALUES ('15', 'F03DD9AC-B586-4FE1-A75B-3E2967BD0207', 'MultiLineText',
-            'A multiline text box', '', FALSE, '9', '1');
-
-INSERT INTO property_type
-    (hid, id, name, description, label, deprecated, display_order, value_type_id)
-    VALUES ('11', 'pt67bdfe81-361e-41fe-852a-35159e1e7bc5', 'XBasePairs',
-            'A text box with caption (x=user specified number)', NULL, TRUE, '4',
+            'A list for selecting a value (numeric range)', NULL, TRUE, '999',
             '3');
 
 INSERT INTO property_type
     (hid, id, name, description, label, deprecated, display_order, value_type_id)
+    VALUES ('15', 'F03DD9AC-B586-4FE1-A75B-3E2967BD0207', 'MultiLineText',
+            'A multiline text box', '', FALSE, '2', '1');
+
+INSERT INTO property_type
+    (hid, id, name, description, label, deprecated, display_order, value_type_id)
+    VALUES ('11', 'pt67bdfe81-361e-41fe-852a-35159e1e7bc5', 'XBasePairs',
+            'A text box with caption (x=user specified number)', NULL, TRUE,
+            '999', '3');
+
+INSERT INTO property_type
+    (hid, id, name, description, label, deprecated, display_order, value_type_id)
     VALUES ('16', '871AA217-2E6C-48E2-880A-EE7815E8F7F8', 'XBasePairsText',
-            'A text box with caption (x=user specified text)', '', TRUE, '4', '1');
+            'A text box with caption (x=user specified text)', '', TRUE, '999',
+            '1');
 
 INSERT INTO property_type
     (hid, id, name, description, label, deprecated, display_order, value_type_id)
     VALUES ('17', 'C00ED92F-5399-490C-A6E5-AAD0E140D7FE', 'Input',
-            'Input file or folder', NULL, FALSE, '999', '4');
+            'Input file or folder', NULL, TRUE, '999', '4');
 
 INSERT INTO property_type
     (hid, id, name, description, label, deprecated, display_order, value_type_id)
     VALUES ('18', 'F7007237-040D-4253-9323-CAA4F71E9795', 'Output',
-            'Output file or folder', NULL, FALSE, '999', '5');
+            'Output file or folder', NULL, FALSE, '11', '5');
 
 INSERT INTO property_type
     (hid, id, name, description, label, deprecated, display_order, value_type_id)
     VALUES ('19', 'A024716E-1F18-4AF7-B59E-0745786D1B69', 'EnvironmentVariable',
             'An environment variable that is set before running a job', NULL,
-            FALSE, '999', '6');
+            FALSE, '9', '6');
 
 INSERT INTO property_type
     (hid, id, name, description, label, deprecated, display_order, value_type_id)
     VALUES ('20', '548A55C2-53FE-40A5-AD38-033F79C8C0AB', 'TreeSelection',
             'A hierarchical list for selecting a choice', NULL,
             FALSE, '10', '1');
+
+INSERT INTO property_type
+    (hid, id, name, description, label, deprecated, display_order, value_type_id)
+    VALUES ('21', 'C389D80A-F94E-4904-B6EF-BD658A18FC8A', 'Integer',
+            'An integer value.', NULL, FALSE, '4', '3');
+
+INSERT INTO property_type
+    (hid, id, name, description, label, deprecated, display_order, value_type_id)
+    VALUES ('22', '01250DB2-F8E9-4D9E-B82E-C4713DA84068', 'Double',
+            'A real number value.', NULL, FALSE, '5', '3');
+
+INSERT INTO property_type
+    (hid, id, name, description, label, deprecated, display_order, value_type_id)
+    VALUES ('23', 'C529C00A-8B6F-4B73-80DA-C460C09722ED', 'TextSelection',
+            'A list for selecting a textual value.', NULL, FALSE, '6', '1');
+
+INSERT INTO property_type
+    (hid, id, name, description, label, deprecated, display_order, value_type_id)
+    VALUES ('24', '0F4E0460-893B-4724-BC7C-D145575B9B73', 'IntegerSelection',
+            'A list for selecting an integer value.', NULL, FALSE, '7', '3');
+
+INSERT INTO property_type
+    (hid, id, name, description, label, deprecated, display_order, value_type_id)
+    VALUES ('25', 'B8566277-C368-40E9-8B66-BC1C884CF69B', 'DoubleSelection',
+            'A list for selecting a real number value.', NULL, FALSE, '8', '3');
