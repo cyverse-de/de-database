56f450bc04f145aebda443289fb138125fc7b411
Paul Sarando <psarando@iplantcollaborative.org>
2013-09-04 16:50:49 -0700

    CORE-4720 Fixed app_count function results.
    Added the DISTINCT clause to the App hid count, since the same App may appear in more than group.

--- src/main/functions/02_app_count.sql
+++ src/main/functions/02_app_count.sql
@@ -5,7 +5,7 @@ SET search_path = public, pg_catalog;
 -- at the node with the given identifier.
 --
 CREATE FUNCTION app_count(bigint) RETURNS bigint AS $$
-    SELECT COUNT(a.hid) FROM transformation_activity a
+    SELECT COUNT(DISTINCT a.hid) FROM transformation_activity a
     JOIN template_group_template tgt on a.hid = tgt.template_id
     WHERE NOT a.deleted
     AND tgt.template_group_id in (SELECT * FROM app_group_hierarchy_ids($1))
