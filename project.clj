38029cff1f9c6f644037c8c5d3a0ee90fc13759e
Dennis Roberts <dennis@iplantcollaborative.org>
2014-02-06 17:54:39 -0700

    CORE-5133: removed project.clj, which wasn't being used

--- project.clj
+++
@@ -1,8 +0,0 @@
-;; This is a fake project used to make it easier to test the database
-;; conversion scripts.
-
-(defproject org.iplantc/de-database-schema
-  :description "Database initialization and conversion scripts."
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [korma "0.3.0-RC4"]
-                 [postgresql "9.0-801.jdbc4"]])
