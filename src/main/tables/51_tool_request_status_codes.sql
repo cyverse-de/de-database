877562e375b5676d78b659eecd646f6d824cd43a
Dennis Roberts <dennis@iplantcollaborative.org>
2014-02-27 19:00:11 -0700

    CORE-4802: converted tool request status code IDs to UUIDs

--- src/main/tables/51_tool_request_status_codes.sql
+++ src/main/tables/51_tool_request_status_codes.sql
@@ -1,20 +1,10 @@
 SET search_path = public, pg_catalog;
 
 --
--- The identifier sequence for the tool_request_status_codes table.
---
-CREATE SEQUENCE tool_request_status_codes_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MAXVALUE
-    NO MINVALUE
-    CACHE 1;
-
---
 -- A table defining the set of available tool request status codes.
 --
 CREATE TABLE tool_request_status_codes (
-    id BIGINT DEFAULT nextval('tool_request_status_codes_id_seq'::regclass) NOT NULL,
+    id UUID NOT NULL,
     name VARCHAR(64) NOT NULL,
     description VARCHAR(128) NOT NULL,
     PRIMARY KEY(id)
--- src/main/tables/54_tool_request_statuses.sql
+++ src/main/tables/54_tool_request_statuses.sql
@@ -16,7 +16,7 @@ CREATE SEQUENCE tool_request_statuses_id_seq
 CREATE TABLE tool_request_statuses (
     id BIGINT DEFAULT nextval('tool_request_statuses_id_seq'::regclass) NOT NULL,
     tool_request_id BIGINT REFERENCES tool_requests(id) NOT NULL,
-    tool_request_status_code_id BIGINT REFERENCES tool_request_status_codes(id) NOT NULL,
+    tool_request_status_code_id UUID REFERENCES tool_request_status_codes(id) NOT NULL,
     date_assigned TIMESTAMP DEFAULT now() NOT NULL,
     updater_id BIGINT NOT NULL,
     comments TEXT,
