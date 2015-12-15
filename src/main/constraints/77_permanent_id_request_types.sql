358e64e51347972c11022d342dc97c4cee8b95fe
Paul Sarando <psarando@iplantcollaborative.org>
2015-12-15 14:07:02 -0700

    CORE-7372 Move permanent_id_request* db tables.
    Moved these tables to the metadata db.

--- src/main/constraints/77_permanent_id_request_types.sql
+++
@@ -1,7 +0,0 @@
-SET search_path = public, pg_catalog;
-
---
--- All Permanent ID types should be unique.
---
-CREATE UNIQUE INDEX permanent_id_request_types_unique
-    ON permanent_id_request_types(type);
--- src/main/data/20_permanent_id_request_types.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO permanent_id_request_types (type, description) VALUES
-  ('DOI',
-   'Data Object Identifier'),
-  ('ARK',
-   'Archival Resource Key');
