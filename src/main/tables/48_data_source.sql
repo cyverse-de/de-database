32a1a43f12ca89be004dd0a8bcd4e269f24af988
Dennis Roberts <dennis@cyverse.org>
2017-01-26 15:37:51 -0700

    CORE-8566: added the `display_order` column to the init scripts for the `data_source` table

--- src/main/tables/48_data_source.sql
+++ src/main/tables/48_data_source.sql
@@ -7,6 +7,6 @@ CREATE TABLE data_source (
     id uuid NOT NULL DEFAULT uuid_generate_v1(),
     name varchar(50) NOT NULL,
     label varchar(50) NOT NULL,
-    description text NOT NULL
+    description text NOT NULL,
+    display_order bigint
 );
-
