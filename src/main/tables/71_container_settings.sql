2e900d514460cecdd92a0930c6b686c0865f34f3
John Wregglesworth <wregglej@iplantcollaborative.org>
2017-05-08 10:31:57 -0700

    Add resource restriction columns to container_settings
    Added the min_memory_limit column.
    Added the min_cpu_cores column.
    Added the min_disk_space column.

--- src/main/tables/71_container_settings.sql
+++ src/main/tables/71_container_settings.sql
@@ -15,9 +15,21 @@ CREATE TABLE container_settings (
   -- The 'shares' of the CPU that the container owns.
   cpu_shares integer,
 
-  -- The number of bytes to limit RAM to. integer isn't big enough, though
-  -- bigint is overkill.
+  -- The maximum RAM usage limit, in bytes.
   memory_limit bigint,
+
+  -- The minimum number of bytes required to run the container. This is used
+  -- in the Condor ClassAds and is not passed to the Docker container.
+  min_memory_limit bigint,
+
+  -- The minimum number of cpu cores required to run the container. It's passed
+  -- to Docker with the --cpus command-line switch.
+  min_cpu_cores integer,
+
+  -- The minimum about of disk space required to run the container. This does
+  -- not correspond to a docker option, it's purely used for Condor job
+  -- matching with ClassAds.
+  min_disk_space bigint,
 
   -- Most likely going to be 'bridge' or 'none', but may be set to a container
   -- id which is why we're using text.
