48b1131232ce0d1b342642b73fc2e061df34cd9c
Sriram Srinivasan <sriram@cyverse.org>
2017-09-01 13:43:02 -0700

    de-1129 add database conversion

---
+++ src/main/conversions/v2.16.0/c2_16_0_20170901.clj
@@ -0,0 +1,20 @@
+(ns facepalm.c2-16-0-20170901
+  (:use [kameleon.sql-reader :only [load-sql-file]]))
+
+(def ^:private version
+  "The destination database version"
+  "2.16.0:20170901.01")
+
+(defn- add-de-webhooks-table
+  "Add the de-webhooks table"
+  []
+  (println "\t* add de-webhooks table...")
+  (load-sql-file "tables/78_webhooks.sql")
+  (load-sql-file "constraints/00_78_webhooks_pkey.sql")
+  (load-sql-file "constraints/78_webhooks.sql"))
+
+(defn convert
+  "Performs the conversion for this database version"
+  []
+  (println "Performing the conversion for" version)
+  (add-de-webhooks-table))
