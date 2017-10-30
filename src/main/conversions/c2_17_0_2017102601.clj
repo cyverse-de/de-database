bc6cb874e229b2c773c0cc0f366d9457e54753c6
Dennis Roberts <dennis@cyverse.org>
2017-10-30 10:57:25 -0700

    CORE-9054: refactored the c2_17_0_2017102601.clj conversion script

--- src/main/conversions/c2_17_0_2017102601.clj
+++ src/main/conversions/c2_17_0_2017102601.clj
@@ -10,15 +10,8 @@
   []
   (println "\t* adding cascading deletions to the template_attrs table.")
   (exec-raw "ALTER TABLE ONLY template_attrs DROP CONSTRAINT template_attrs_template_id_fkey")
-  (exec-raw "ALTER TABLE ONLY template_attrs
-             ADD CONSTRAINT template_attrs_template_id_fkey
-             FOREIGN KEY (template_id)
-             REFERENCES templates(id) ON DELETE CASCADE")
   (exec-raw "ALTER TABLE ONLY template_attrs DROP CONSTRAINT template_attrs_attribute_id_fkey")
-  (exec-raw "ALTER TABLE ONLY template_attrs
-             ADD CONSTRAINT template_attrs_attribute_id_fkey
-             FOREIGN KEY (attribute_id)
-             REFERENCES attributes(id) ON DELETE CASCADE"))
+  (kameleon.sql-reader/load-sql-file "constraints/06_template_attrs.sql"))
 
 (defn convert
   "Performs the conversion for this database version"
