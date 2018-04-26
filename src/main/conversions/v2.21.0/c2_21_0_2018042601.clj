8cdb070a6be374dd5ef1fd19ceaff3aa5d8387f6
John Wregglesworth <wregglej@iplantcollaborative.org>
2018-04-26 10:46:04 -0700

    Add conversion for interactive apps changes

---
+++ src/main/conversions/v2.21.0/c2_21_0_2018042601.clj
@@ -0,0 +1,25 @@
+(ns facepalm.c2-21-0-2018042601
+  (:use [kameleon.sql-reader :only [load-sql-file exec-sql-statement]]))
+
+(def ^:private version
+  "The destination database version."
+  "2.21.0:20180426.01")
+
+(defn- add-interactive-column
+  "Adds the interactive column to the tools table"
+  []
+  (exec-sql-statement
+   "ALTER TABLE ONLY tools
+    ADD COLUMN interactive NOT NULL DEFAULT FALSE"))
+
+(defn- create-container-ports-table
+  "Adds the container_ports table to the database"
+  []
+  (load-sql-file "tables/83_container_ports.sql"))
+
+(defn convert
+  "Performs the conversion for this database version."
+  []
+  (println "Performing the conversion for" version)
+  (add-interactive-column)
+  (craete-container-ports-table))
