43b8b2f0579f387311135ea92c4069276e558478
John Wregglesworth <wregglej@iplantcollaborative.org>
2018-06-01 11:10:00 -0700

    Add the interactive job type

--- src/main/data/18_job_types.sql
+++ src/main/data/18_job_types.sql
@@ -2,4 +2,5 @@
 
 INSERT INTO job_types(id, name, system_id) VALUES
     ( 'AD069D9F-E38F-418C-84F6-21F620CADE77', 'DE', 'de' ),
-    ( '61433582-A271-4154-B3C5-F4C1D91DB2A4', 'Agave', 'agave' );
+    ( '61433582-A271-4154-B3C5-F4C1D91DB2A4', 'Agave', 'agave' ),
+    ( 'EAD7467A-67C1-4087-90E1-F29EBF2EA084', 'Interactive', 'interactive');
