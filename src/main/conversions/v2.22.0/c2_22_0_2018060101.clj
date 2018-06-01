1145ef953a0563c9f0b2bd8cddc05c08e0158971
John Wregglesworth <wregglej@iplantcollaborative.org>
2018-06-01 12:15:19 -0700

    Changed the version back to v2.22

--- src/main/conversions/v2.22.0/c2_22_0_2018060101.clj
+++ src/main/conversions/v2.22.0/c2_22_0_2018060101.clj
@@ -1,9 +1,9 @@
-(ns facepalm.c2-23-0-2018060101
+(ns facepalm.c2-22-0-2018060101
   (:use [kameleon.sql-reader :only [exec-sql-statement]]))
 
 (def ^:private version
   "The destination database version"
-  "2.23.0:20180601.01")
+  "2.22.0:20180601.01")
 
 (defn- add-interactive-job-type
   "Adds a record for an interactive job type."
