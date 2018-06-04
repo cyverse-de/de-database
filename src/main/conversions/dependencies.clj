111b1ccc2447cfc2315042092b7eb05d3e94104b
John Wregglesworth <wregglej@iplantcollaborative.org>
2018-06-04 09:13:51 -0700

    Add the github.com/cyverse-de/mvn repo.
    Some of the dependencies we need for jargon are no longer available in
    the renci repo, so they've been mirrored on github.
    
    The repositories section in dependencies.clj was added as a map since
    that's all that facepalm seems to be able to handle.

--- src/main/conversions/dependencies.clj
+++ src/main/conversions/dependencies.clj
@@ -1,2 +1,4 @@
 {:dependencies [[org.clojure/java.jdbc "0.3.5"]
-                [org.cyverse/clj-jargon "2.8.0"]]}
+                [org.cyverse/clj-jargon "2.8.0"]]
+ :repositories {"cyverse-de"
+                {:url "https://raw.github.com/cyverse-de/mvn/master/releases"}}}
