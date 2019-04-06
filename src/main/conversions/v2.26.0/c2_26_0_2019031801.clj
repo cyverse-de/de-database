cc219e082af57db83fdee87754435435e6af16f9
Sarah Roberts <sarahr@cyverse.org>
2019-04-05 19:30:33 -0700

    modified c2_26_0_2019031801.clj so that it doesn't attempt to modify the badges table if it doesn't exist

--- src/main/conversions/v2.26.0/c2_26_0_2019031801.clj
+++ src/main/conversions/v2.26.0/c2_26_0_2019031801.clj
@@ -25,7 +25,7 @@
    "ALTER TABLE IF EXISTS ONLY badges
     RENAME CONSTRAINT badges_user_id_fkey TO quick_launches_creator_fkey;")
   (exec-sql-statement
-   "ALTER TABLE ONLY badges
+   "ALTER TABLE IF EXISTS ONLY badges
     ADD CONSTRAINT quick_launches_app_id_fkey FOREIGN KEY (app_id) REFERENCES apps(id);")
   (exec-sql-statement
    "ALTER TABLE IF EXISTS ONLY badges RENAME TO quick_launches;"))
