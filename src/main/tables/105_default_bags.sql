399fa0a0866bc1b03d2ebe68fca5f01c96b971e7
John Wregglesworth <wregglej@cyverse.org>
2020-09-02 11:30:46 -0700

    Add uniqueness constraints on the user_id and bag_id columns of the default_bags table

--- src/main/tables/105_default_bags.sql
+++ src/main/tables/105_default_bags.sql
@@ -4,7 +4,7 @@ SET search_path = public, pg_catalog;
 -- Tracks default bags.
 --
 CREATE TABLE IF NOT EXISTS default_bags (
-    user_id UUID NOT NULL,
+    user_id UUID UNIQUE NOT NULL,
 
-    bag_id UUID NOT NULL
+    bag_id UUID UNIQUE NOT NULL
 )
