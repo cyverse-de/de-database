b7073b1d9bad0f9f0d15ab8dfdb5d672a20253ee
Ian McEwen <mian@cyverse.org>
2021-01-20 09:34:05 -0700

    Add IF NOT EXISTS for idempotency

--- migrations/000001_users_table.up.sql
+++ migrations/000001_users_table.up.sql
@@ -1,6 +1,6 @@
 SET search_path = public, pg_catalog;
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id uuid NOT NULL DEFAULT uuid_generate_v1(),
     username character varying(512) NOT NULL UNIQUE,
     PRIMARY KEY (id)
