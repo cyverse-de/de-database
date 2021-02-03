b337bacdd1cc28e632366898f36c3845ad6e66d4
Ian McEwen <mian@cyverse.org>
2021-02-02 19:50:01 -0700

    Add tools migration

---
+++ migrations/000008_tools.down.sql
@@ -0,0 +1,18 @@
+SET search_path = public, pg_catalog;
+
+DROP TABLE IF EXISTStool_test_data_files;
+
+DROP TABLE IF EXISTStool_type_parameter_type;
+
+DROP TABLE IF EXISTStool_request_statuses;
+DROP TABLE IF EXISTStool_request_status_codes;
+DROP TABLE IF EXISTStool_requests;
+DROP TABLE IF EXISTStool_architectures;
+
+DROP TABLE IF EXISTScontainer_volumes;
+DROP TABLE IF EXISTScontainer_devices;
+DROP TABLE IF EXISTScontainer_ports;
+DROP TABLE IF EXISTScontainer_volumes_from;
+DROP TABLE IF EXISTSdata_containers;
+DROP TABLE IF EXISTScontainer_settings;
+DROP TABLE IF EXISTSinteractive_apps_proxy_settings;
