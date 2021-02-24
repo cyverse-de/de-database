adaee5eb6d198a25c0586da2d1c02c08a34632b1
Ian McEwen <mian@cyverse.org>
2021-02-24 09:25:58 -0700

    idempotence for beta-part-of-public-apps-group

--- migrations/000019_data_fixtures.up.sql
+++ migrations/000019_data_fixtures.up.sql
@@ -559,10 +559,10 @@ INSERT INTO app_category_group (parent_category_id, child_category_id, child_ind
        FROM app_categories parent, app_categories child
        WHERE parent.id = '12c7a585-ec23-3352-e313-02e323112a7c'
        AND child.id = '5401bd146c144470aedd57b47ea1b979';
+    ON CONFLICT (parent_category_id, child_category_id) DO UPDATE SET child_index=EXCLUDED.child_index;
 
 UPDATE workspace SET root_category_id = '12c7a585-ec23-3352-e313-02e323112a7c'
        WHERE id = '00000000-0000-0000-0000-000000000000';
-
 
 --This is the reference_genomes.json from metadactly to be autopopulated in the
 --postgresDB in the genome_reference table.
