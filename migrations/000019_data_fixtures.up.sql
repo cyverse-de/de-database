da014fdeedfd8e87ddc84b8191c0ebdca5793dac
Ian McEwen <mian@cyverse.org>
2021-02-24 08:44:56 -0700

    compact and upsertize value_type

--- migrations/000019_data_fixtures.up.sql
+++ migrations/000019_data_fixtures.up.sql
@@ -100,29 +100,21 @@ INSERT INTO info_type (id, name, label, description, deprecated, display_order) 
 
 -- value_type
 
-INSERT INTO value_type (id, name, description)
-    VALUES ('0115898A-F81A-4598-B1A8-06E538F1D774', 'String',
-            'Arbitrary text');
-
-INSERT INTO value_type (id, name, description)
-    VALUES ('E8E05E6C-5002-48C0-9167-C9733F0A9716', 'Boolean',
-            'True or false value');
-
-INSERT INTO value_type (id, name, description)
-    VALUES ('BCB81292-F01D-45B1-8598-3D6CD745D2E9', 'Number',
-            'Numeric value');
-
-INSERT INTO value_type (id, name, description)
-    VALUES ('94FE4F2B-42F9-4EE7-BC28-F64CC8DAF691', 'Input',
-            'Input file or folder');
-
-INSERT INTO value_type (id, name, description)
-    VALUES ('65E1E927-A1ED-4E25-BC0C-74C155215973', 'Output',
-            'Output file or folder');
-
-INSERT INTO value_type (id, name, description)
-    VALUES ('96DE7B1E-FE29-468F-85C0-A9458CE66FB1', 'EnvironmentVariable',
-            'An environment variable that is set before running a job');
+INSERT INTO value_type (id, name, description) VALUES
+    ('0115898A-F81A-4598-B1A8-06E538F1D774', 'String',
+           'Arbitrary text'),
+    ('E8E05E6C-5002-48C0-9167-C9733F0A9716', 'Boolean',
+           'True or false value'),
+    ('BCB81292-F01D-45B1-8598-3D6CD745D2E9', 'Number',
+           'Numeric value'),
+    ('94FE4F2B-42F9-4EE7-BC28-F64CC8DAF691', 'Input',
+           'Input file or folder'),
+    ('65E1E927-A1ED-4E25-BC0C-74C155215973', 'Output',
+           'Output file or folder'),
+    ('96DE7B1E-FE29-468F-85C0-A9458CE66FB1', 'EnvironmentVariable',
+           'An environment variable that is set before running a job')
+    ON CONFLICT (id) DO UPDATE
+        SET name=EXCLUDED.name, description=EXCLUDED.description;
 
 -- rule_subtype
 
