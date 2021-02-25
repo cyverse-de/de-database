ac02fdd99685da8cece7ab649b3e00b42e08aa84
Ian McEwen <mian@cyverse.org>
2021-02-25 11:47:35 -0700

    Make requests up script idempotent

--- migrations/000015_requests.up.sql
+++ migrations/000015_requests.up.sql
@@ -7,8 +7,8 @@ CREATE TABLE IF NOT EXISTS request_types (
     id uuid NOT NULL DEFAULT uuid_generate_v1(),
     name text NOT NULL,
 
-    PRIMARY KEY (id),
-    UNIQUE (name)
+    UNIQUE (name),
+    PRIMARY KEY (id)
 );
 
 -- requests
@@ -18,18 +18,10 @@ CREATE TABLE IF NOT EXISTS requests (
     requesting_user_id uuid NOT NULL,
     details json NOT NULL,
 
+    FOREIGN KEY (request_type_id) REFERENCES request_types(id) ON DELETE CASCADE,
+    FOREIGN KEY (requesting_user_id) REFERENCES users(id) ON DELETE CASCADE,
     PRIMARY KEY (id)
 );
-
-ALTER TABLE requests
-    ADD CONSTRAINT requests_request_type_id_fkey
-    FOREIGN KEY (request_type_id)
-    REFERENCES request_types(id) ON DELETE CASCADE;
-
-ALTER TABLE requests
-    ADD CONSTRAINT requests_requesting_user_id_fkey
-    FOREIGN KEY (requesting_user_id)
-    REFERENCES users(id) ON DELETE CASCADE;
 
 -- request_status_codes
 CREATE TABLE IF NOT EXISTS request_status_codes (
@@ -52,23 +44,12 @@ CREATE TABLE IF NOT EXISTS request_updates (
     created_date timestamp NOT NULL DEFAULT now(),
     message text,
 
+    FOREIGN KEY (request_id) REFERENCES requests(id) ON DELETE CASCADE,
+    FOREIGN KEY (request_status_code_id) REFERENCES request_status_codes(id) ON DELETE CASCADE,
+    FOREIGN KEY (updating_user_id) REFERENCES users(id) ON DELETE CASCADE,
     PRIMARY KEY (id)
 );
 
-ALTER TABLE request_updates
-    ADD CONSTRAINT request_updates_request_id_fkey
-    FOREIGN KEY (request_id)
-    REFERENCES requests(id) ON DELETE CASCADE;
-
-ALTER TABLE request_updates
-    ADD CONSTRAINT request_updates_request_status_code_id_fkey
-    FOREIGN KEY (request_status_code_id)
-    REFERENCES request_status_codes(id) ON DELETE CASCADE;
-
-ALTER TABLE request_updates
-    ADD CONSTRAINT request_updates_updating_user_id_fkey
-    FOREIGN KEY (updating_user_id)
-    REFERENCES users(id) ON DELETE CASCADE;
 --
 -- Index on request ID and timestamp.
 --
