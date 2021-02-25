8a5d61d7d6e3fa57bce9c850111cebb6dc2b3a91
Ian McEwen <mian@cyverse.org>
2021-02-25 10:10:47 -0700

    Move foreign key into table definition for logins migration

--- migrations/000002_logins.up.sql
+++ migrations/000002_logins.up.sql
@@ -7,14 +7,8 @@ CREATE TABLE IF NOT EXISTS logins (
     ip_address varchar(15),
     user_agent text,
     login_time timestamp NOT NULL DEFAULT now(),
-    logout_time timestamp
+    logout_time timestamp,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
-
--- this part probably isn't idempotent, don't think there's an IF NOT EXISTS for ALTER TABLE ADD CONSTRAINT though
-
-ALTER TABLE ONLY logins
-    ADD CONSTRAINT logins_user_id_fkey
-    FOREIGN KEY (user_id)
-    REFERENCES users(id);
 
 COMMIT;
