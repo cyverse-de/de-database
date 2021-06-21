09c54122502dfdbb789bff1a8d716f433dc15784
Sarah Roberts <sarahr@cyverse.org>
2021-06-21 12:05:52 -0700

    CORE-1452: added a maximum_concurrent_requests_per_user column

--- migrations/000023_request_type_limit.down.sql
+++ migrations/000023_request_type_limit.down.sql
@@ -2,8 +2,11 @@ BEGIN;
 
 SET search_path = public, pg_catalog;
 
--- Remove the `maximum_requests_per_user` column from the `request_types` table.
+-- Remove the `maximum_requests_per_user` and `maximum_concurrent_requests_per_user` columns from the `request_types`
+-- table.
 ALTER TABLE IF EXISTS request_types
 DROP COLUMN IF EXISTS maximum_requests_per_user;
+ALTER TABLE IF EXISTS request_types
+DROP COLUMN IF EXISTS maximum_concurrent_requests_per_user;
 
 COMMIT
