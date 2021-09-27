339ef031986efb15feb472b642141e549d2203e9
John Wregglesworth <wregglej@cyverse.org>
2021-09-27 13:24:58 -0700

    Fcpu_usage_events_processed_on_trigger should work on processed_on field

--- migrations/000025_cpu_usage_workers.up.sql
+++ migrations/000025_cpu_usage_workers.up.sql
@@ -4,7 +4,7 @@ SET search_path = public, pg_catalog;
 
 ALTER TABLE IF EXISTS ONLY cpu_usage_events 
     ADD IF NOT EXISTS claimed boolean NOT NULL DEFAULT false,
-    ADD IF NOT EXISTS claimed_by UUID REFERENCES users(id),
+    ADD IF NOT EXISTS claimed_by character varying)32),
     ADD IF NOT EXISTS claimed_on timestamp,
     ADD IF NOT EXISTS claim_expires_on timestamp,
     ADD IF NOT EXISTS processed boolean NOT NULL DEFAULT false,
@@ -25,6 +25,6 @@ CREATE TRIGGER cpu_usage_events_processed_on_trigger
     AFTER UPDATE ON cpu_usage_events
     FOR EACH ROW
     WHEN (NOT OLD.processed AND NEW.processed)
-    EXECUTE PROCEDURE moddatetime (claimed_on);
+    EXECUTE PROCEDURE moddatetime (processed_on);
 
 COMMIT;
