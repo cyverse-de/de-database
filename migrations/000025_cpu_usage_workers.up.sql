1bfd2bcca4bbd81caac8f5b0d2c2ecc97ca91118
John Wregglesworth <wregglej@cyverse.org>
2021-09-29 12:30:29 -0700

    Make cpu_usage_workers(id) a PK, make cpu_usage_events(claimed_by) an FK, rearrange order

--- migrations/000025_cpu_usage_workers.up.sql
+++ migrations/000025_cpu_usage_workers.up.sql
@@ -2,34 +2,8 @@ BEGIN;
 
 SET search_path = public, pg_catalog;
 
-ALTER TABLE IF EXISTS ONLY cpu_usage_events 
-    ADD IF NOT EXISTS claimed boolean NOT NULL DEFAULT false,
-    ADD IF NOT EXISTS claimed_by character varying(32),
-    ADD IF NOT EXISTS claimed_on timestamp,
-    ADD IF NOT EXISTS claim_expires_on timestamp,
-    ADD IF NOT EXISTS processed boolean NOT NULL DEFAULT false,
-    ADD IF NOT EXISTS processing boolean NOT NULL DEFAULT false,
-    ADD IF NOT EXISTS processed_on timestamp,
-    ADD IF NOT EXISTS max_processing_attempts int NOT NULL DEFAULT 3,
-    ADD IF NOT EXISTS attempts int NOT NULL DEFAULT 0;
-
-DROP TRIGGER IF EXISTS cpu_usage_events_claimed_on_trigger ON cpu_usage_events CASCADE;
-CREATE TRIGGER cpu_usage_events_claimed_on_trigger
-    AFTER UPDATE ON cpu_usage_events
-    FOR EACH ROW
-    WHEN (NOT OLD.claimed AND NEW.claimed)
-    EXECUTE PROCEDURE moddatetime (claimed_on);
-
-DROP TRIGGER IF EXISTS cpu_usage_events_processed_on_trigger ON cpu_usage_events CASCADE;
-CREATE TRIGGER cpu_usage_events_processed_on_trigger
-    AFTER UPDATE ON cpu_usage_events
-    FOR EACH ROW
-    WHEN (NOT OLD.processed AND NEW.processed)
-    EXECUTE PROCEDURE moddatetime (processed_on);
-
-
 CREATE TABLE IF NOT EXISTS cpu_usage_workers (
-    id uuid NOT NULL DEFAULT uuid_generate_v1(),
+    id uuid UNIQUE NOT NULL DEFAULT uuid_generate_v1(),
     added_on timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     name character varying(32) NOT NULL,
     active bool NOT NULL DEFAULT true,
@@ -41,7 +15,9 @@ CREATE TABLE IF NOT EXISTS cpu_usage_workers (
     getting_work_expires_on timestamp,
     working bool NOT NULL DEFAULT false,
     working_on timestamp,
-    last_modified timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+    last_modified timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY(id)
 );
 
 DROP TRIGGER IF EXISTS cpu_usage_workers_last_modified_trigger ON cpu_usage_workers CASCADE;
@@ -70,5 +46,30 @@ CREATE TRIGGER cpu_usage_workers_getting_work_on_trigger
     FOR EACH ROW
     WHEN (NOT OLD.getting_work AND NEW.getting_work)
     EXECUTE PROCEDURE moddatetime (getting_work_on);
+
+ALTER TABLE IF EXISTS ONLY cpu_usage_events 
+    ADD IF NOT EXISTS claimed boolean NOT NULL DEFAULT false,
+    ADD IF NOT EXISTS claimed_by uuid REFERENCES cpu_usage_workers(id),
+    ADD IF NOT EXISTS claimed_on timestamp,
+    ADD IF NOT EXISTS claim_expires_on timestamp,
+    ADD IF NOT EXISTS processed boolean NOT NULL DEFAULT false,
+    ADD IF NOT EXISTS processing boolean NOT NULL DEFAULT false,
+    ADD IF NOT EXISTS processed_on timestamp,
+    ADD IF NOT EXISTS max_processing_attempts int NOT NULL DEFAULT 3,
+    ADD IF NOT EXISTS attempts int NOT NULL DEFAULT 0;
+
+DROP TRIGGER IF EXISTS cpu_usage_events_claimed_on_trigger ON cpu_usage_events CASCADE;
+CREATE TRIGGER cpu_usage_events_claimed_on_trigger
+    AFTER UPDATE ON cpu_usage_events
+    FOR EACH ROW
+    WHEN (NOT OLD.claimed AND NEW.claimed)
+    EXECUTE PROCEDURE moddatetime (claimed_on);
+
+DROP TRIGGER IF EXISTS cpu_usage_events_processed_on_trigger ON cpu_usage_events CASCADE;
+CREATE TRIGGER cpu_usage_events_processed_on_trigger
+    AFTER UPDATE ON cpu_usage_events
+    FOR EACH ROW
+    WHEN (NOT OLD.processed AND NEW.processed)
+    EXECUTE PROCEDURE moddatetime (processed_on);
 
 COMMIT;
