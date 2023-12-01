10e4e60abd28f08b6ad8025abc3acdfab31ef34a
Sarah Roberts <sarahr@cyverse.org>
2023-12-01 16:45:16 -0700

    fixed a regular expression escape character in 000004_basic_job_system.up.sql

--- migrations/000004_basic_job_system.up.sql
+++ migrations/000004_basic_job_system.up.sql
@@ -7,8 +7,8 @@ SET search_path = public, pg_catalog;
 --
 CREATE TABLE IF NOT EXISTS integration_data (
     id uuid NOT NULL DEFAULT uuid_generate_v1(),
-    integrator_name character varying(255) NOT NULL CHECK (integrator_name ~ '\S'),
-    integrator_email character varying(255) NOT NULL CHECK (integrator_email ~ '\S'),
+    integrator_name character varying(255) NOT NULL CHECK (integrator_name ~ '\\S'),
+    integrator_email character varying(255) NOT NULL CHECK (integrator_email ~ '\\S'),
     user_id uuid,
     UNIQUE (integrator_name, integrator_email),
     FOREIGN KEY (user_id) REFERENCES users (id),
