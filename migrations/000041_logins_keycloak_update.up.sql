fe18c517321fb3afc3ff3a1ae601b9f15d2d3631
Ian McEwen <mian@cyverse.org>
2024-12-02 10:43:33 -0700

    also drop logout_time column

--- migrations/000041_logins_keycloak_update.up.sql
+++ migrations/000041_logins_keycloak_update.up.sql
@@ -4,6 +4,7 @@ SET search_path = public, pg_catalog;
 
 ALTER TABLE ONLY logins
  DROP COLUMN IF EXISTS user_agent,
+ DROP COLUMN IF EXISTS logout_time,
   ADD COLUMN IF NOT EXISTS session_id uuid;
 
 COMMIT;
